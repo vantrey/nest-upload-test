6ff0550e0bdde7dd2d52370dc2a06845521c816e
Alexsandr3 <fam_86@mail.ru>
2023-02-01 14:01:30 +0100

    added sorBy for questions

--- src/modules/quiz/infrastructure/quiz-repositories.ts
+++ src/modules/quiz/infrastructure/quiz-repositories.ts
@@ -90,7 +90,7 @@ export class QuizRepositories {
     return this.questionRepo.find({
       select: ['id', 'body'],
       where: { published: true },
-      // order: { createdAt: 'ASC' },
+      order: { createdAt: 'ASC' },
       take: 5,
     });
     // .createQueryBuilder('q')
