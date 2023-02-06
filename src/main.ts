b566382281ccff8681fad4b9fcaa03a9c9a51f18
Alexsandr3 <fam_86@mail.ru>
2023-02-06 20:22:04 +0100

    exchange swagger statics

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,31 @@ async function bootstrap() {
   await finishedApp.listen(port).then(async () => console.log(`Server is listening on ${await app.getUrl()}`));
   // get the swagger json file (if app is running in development mode)
   const dev = configService.get('dev', { infer: true });
+  /*if (dev.NODE_ENV === 'development') {
+    const serverUrl = `http://localhost:${port}`;
+    // write swagger ui files
+    get(`${serverUrl}/api/swagger-ui-bundle.js`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
+      // console.log(`Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`);
+    });
+
+    get(`${serverUrl}/api/swagger-ui-init.js`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
+      // console.log(`Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`);
+    });
+
+    get(`${serverUrl}/api/swagger-ui-standalone-preset.js`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui-standalone-preset.js'));
+      // console.log(`Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`);
+    });
+
+    get(`${serverUrl}/api/swagger-ui.css`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
+      // console.log(`Swagger UI css file written to: '/swagger-static/swagger-ui.css'`);
+    });
+  }*/
+
+  // get the swagger json file (if app is running in development mode)
   if (dev.NODE_ENV === 'development') {
     const serverUrl = `http://localhost:${port}`;
     // write swagger ui files
